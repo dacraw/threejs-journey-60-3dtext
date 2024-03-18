f0cd7b9f91b96dd664afefbcc61d83e4253dd7bd
Doug <doug.a.crawford@gmail.com>
2024-03-18 13:46:42 -0700

    Add 3d text

---
+++ src/Experience.jsx
@@ -0,0 +1,17 @@
+import { OrbitControls, Text3D } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+
+export default function Experience() {
+  return (
+    <>
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      <Text3D font="./fonts/helvetiker_regular.typeface.json">
+        Hello R3F
+        <meshNormalMaterial />
+      </Text3D>
+    </>
+  );
+}
